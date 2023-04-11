4dce13f6911f063026c1655db23e7cc42028e59e
ZSergeiiV <sergeii_@mail.ru>
2023-04-11 23:11:05 +0300

    modified:   lib/features/widgets/cart_widgets.dart

--- lib/features/widgets/cart_widgets.dart
+++ lib/features/widgets/cart_widgets.dart
@@ -17,73 +17,120 @@ class _PaymentState extends State<Payment> {
     'checkAmount': 0
   };
 
-  void changePageOnPay(StepsPayment step, dynamic param) {
-    switch (step) {
-      case StepsPayment.amountMoneyReceived:
-        payData['paymentType'] = param;
-        break;
-      case StepsPayment.payment:
-        payData['paymentType'] = param;
-        break;
-      default:
-        break;
-    }
-    controller.nextPage(
-        duration: const Duration(milliseconds: 100), curve: Curves.easeIn);
-  }
-
   @override
   Widget build(BuildContext context) {
-    // payData['paymentType'] = '';
-    // payData['bayerType'] = '';
-    // payData['checkAmount'] = 0;
+    // @override
+    // void initState() {
+    //   super.initState();
+    // }
+
+    void changePageOnPay(StepsPayment step, dynamic param) {
+      switch (step) {
+        case StepsPayment.choosPaymentMethod:
+          payData['paymentType'] = param;
+          break;
+        case StepsPayment.choosTypeBuyer:
+          payData['bayerType'] = param;
+          break;
+        default:
+          break;
+      }
+      //controller.jumpTo(value)
+      controller.nextPage(
+          duration: const Duration(milliseconds: 100), curve: Curves.easeIn);
+    }
+
+    _pageChoisePaymentType() {
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Padding(
+            padding: EdgeInsets.only(left: 25, top: 15),
+            child: Text(
+              'Выберите способ оплаты',
+              style: TextStyle(fontSize: 30),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          const SizedBox(height: 20),
+          ListTile(
+              onTap: () => changePageOnPay(
+                  StepsPayment.choosPaymentMethod, CheckPaymentType.cash),
+              style: ListTileStyle.list,
+              leading: const Icon(
+                Icons.money_rounded,
+                size: 70,
+              ),
+              title: const Text('Наличными', style: Payment.stylePayCaption),
+              minVerticalPadding: 30),
+          ListTile(
+              onTap: () => changePageOnPay(
+                  StepsPayment.choosPaymentMethod, CheckPaymentType.card),
+              leading: const Icon(
+                Icons.credit_card_rounded,
+                size: 70,
+              ),
+              title: const Text('Банковской картой',
+                  style: Payment.stylePayCaption),
+              minVerticalPadding: 30)
+        ],
+      );
+    }
+
+    _pageChoiseBayerType() {
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Padding(
+            padding: EdgeInsets.only(left: 25, top: 15),
+            child: Text(
+              'Выберите тип покупателя',
+              style: TextStyle(fontSize: 30),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          const SizedBox(height: 20),
+          ListTile(
+              onTap: () => changePageOnPay(
+                  StepsPayment.choosTypeBuyer, BuyerType.person),
+              style: ListTileStyle.list,
+              leading: const Icon(
+                Icons.person,
+                size: 70,
+              ),
+              title: const Text('Частное лицо', style: Payment.stylePayCaption),
+              minVerticalPadding: 30),
+          ListTile(
+              onTap: () => changePageOnPay(
+                  StepsPayment.choosTypeBuyer, BuyerType.juridicalPerson),
+              leading: const Icon(
+                Icons.business,
+                size: 70,
+              ),
+              title: const Text('Юридическое лицо',
+                  style: Payment.stylePayCaption),
+              minVerticalPadding: 30)
+        ],
+      );
+    }
 
     return PageView(
       controller: controller,
+      physics: const NeverScrollableScrollPhysics(),
       children: <Widget>[
-        Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Padding(
-              padding: EdgeInsets.only(left: 25, top: 15),
-              child: Text(
-                'Выберите способ оплаты',
-                style: TextStyle(fontSize: 30),
-                textAlign: TextAlign.left,
-              ),
-            ),
-            const SizedBox(height: 20),
-            ListTile(
-                onTap: () => changePageOnPay(
-                    StepsPayment.choosPaymentMethod, CheckPaymentType.cash),
-                style: ListTileStyle.list,
-                leading: const Icon(
-                  Icons.money_rounded,
-                  size: 70,
-                ),
-                title: const Text('Наличными', style: Payment.stylePayCaption),
-                minVerticalPadding: 30),
-            ListTile(
-                onTap: () => changePageOnPay(
-                    StepsPayment.choosPaymentMethod, CheckPaymentType.card),
-                leading: const Icon(
-                  Icons.credit_card_rounded,
-                  size: 70,
-                ),
-                title: const Text('Банковской картой',
-                    style: Payment.stylePayCaption),
-                minVerticalPadding: 30)
-          ],
-        ),
-        Container(
-          color: Colors.cyan,
-        ),
+        _pageChoiseBayerType(),
+        _pageChoisePaymentType(),
+
         // Container(
         //   color: Colors.deepPurple,
         // ),
       ],
     );
-    ;
   }
 }
+
+// Widget PageChoisePaymentType(BuildContext context) {
+//   return Column()
+// }
